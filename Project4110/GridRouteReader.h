009ce2bcfe53379260d7066e1372c0cf32346810
Iproiguess <superblade021@gmail.com>
2025-08-10 15:23:26 +0800

    Update GridRouteReader.h

--- Project4110/GridRouteReader.h
+++ Project4110/GridRouteReader.h
@@ -10,6 +10,8 @@ public:
 
     vector<string> gridLoader(const string& filename);
 
+    vector<pair<int, int>> findPath(pair<int, int> start, pair<int, int> goal) const;
+
 private:
     vector<string> grid;
 };
