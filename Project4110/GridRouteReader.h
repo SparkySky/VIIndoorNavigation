17d8e62fda7562ad26b9ad5518cc45220a7d25c7
Iproiguess <superblade021@gmail.com>
2025-08-10 16:30:02 +0800

    Update GridRouteReader.h

--- Project4110/GridRouteReader.h
+++ Project4110/GridRouteReader.h
@@ -12,10 +12,14 @@ public:
 
     vector<string> gridLoader(const string& filename);
 
-    vector<pair<int, int>> findPath(pair<int, int> start, pair<int, int> goal) const;
+    vector<pair<int, int>> findPath(pair<int, int> start, pair<int, int> goal) const; 
 
-    unordered_map<string, pair<int,int>> landmarks;
+    unordered_map<string, pair<int,int>> getLandmarks() const {
+        return landmarks;
+    }
+
 
 private:
     vector<string> grid;
+    unordered_map<string, pair<int,int>> landmarks;
 };
